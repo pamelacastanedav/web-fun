c3f5dfd042d0ddf3f1f94eb5517c2de601caef29
Pamela Castaneda <pamelacastanedav@gmail.com>
2021-10-15 12:23:22 -0700

    init commit

---
+++ DictionaryEntry/Algo-examples/algo.js
@@ -0,0 +1,8 @@
+var arr = ["a", "b", "c", "d", "e"]
+function reverse (arr) {
+   for  (var arr = arr.length - 1; i >= 0; i--)
+
+
+console.log(reverse(["a", "b", "c", "d", "e"])); 
+return var(arr)
+
