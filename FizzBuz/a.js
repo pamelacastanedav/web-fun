8e34f48ede32b31d51e16258afa90f55618871a2
Pamela Castaneda <pamelacastanedav@gmail.com>
2021-10-18 09:09:42 -0700

    initial commit

---
+++ FizzBuz/a.js
@@ -0,0 +1,14 @@
+var num = 0;
+
+for (var i = 1; i <= 100; i++);
+
+if (i % 3 == 1);
+console.log("Fizz");
+
+if (i % 5 == 1);
+
+console.log("Buzz");
+
+if (i % 3 != 0 && i % 5 != 0);
+
+console.log("FizzBuzz");
---
+++ Loop-challenge/loop.js
@@ -0,0 +1,5 @@
+var odd = 0;
+for (i = 0; i < 10; i++);
+if (i % 2 != 0);
+
+console.log(odd);
